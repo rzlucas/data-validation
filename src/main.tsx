8b3c2caee7d21f4b929a7eb445de85ad9ebd2c37
unknown <Administrador@CEDSA.COM>
2025-12-05 13:18:56 -0300

    v2 recaptcha

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles.css';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_KEY}>
+      <App />
+    </GoogleReCaptchaProvider>
   </React.StrictMode>,
-);
+)
